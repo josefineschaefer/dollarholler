7378caa2e3690439b711c8099ce46b5e9ea8d581
josefineschaefer <josefine.schaefer@storyblok.com>
2022-12-10 18:06:26 +0100

    make navbar responsive and add icons

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -24,6 +24,10 @@ module.exports = {
         gallery: '#efeaf2',
         caribbeanGreen: '#00ca9e',
         fog: '#decaec'
+      },
+      zIndex: {
+        navBarToggle: 91,
+        navBar: 90
       }
     }
   },
