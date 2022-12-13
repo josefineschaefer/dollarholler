9eed93dc710365a7f4949fa6027a5e85fd20cc34
josefineschaefer <josefine.schaefer@storyblok.com>
2022-12-13 16:30:59 +0100

    add-icons

---
+++ src/lib/components/Search.svelte
@@ -0,0 +1,15 @@
+<script lang="typescript">
+  import Search from '$lib/components/icons/Search.svelte';
+</script>
+
+<div class="relative flex w-full items-basline">
+  <div class="mr-2 text-pastelPurple"><Search /></div>
+  <input
+    type="text"
+    name="search"
+    placeholder="search by keyword"
+    class="w-72 border-b-2 border-dashed border-b-pastelPurple bg-transparent front-sansSerif text-black text-xl outline-none focus:border-b-lavenderIndigo focus:border-solid"
+  />
+  <button class="font-sansSerif text-xl font-black text-pastelPurple">Search</button>
+  <div />
+</div>
--- src/routes/(dashboard)/invoices/+page.svelte
+++ src/routes/(dashboard)/invoices/+page.svelte
@@ -1,43 +1,42 @@
-Invoices
-<p>
-  "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut
-  labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris
-  nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit
-  esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt
-  in culpa qui officia deserunt mollit anim id est laborum."
-</p>
-<p>
-  "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut
-  labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris
-  nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit
-  esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt
-  in culpa qui officia deserunt mollit anim id est laborum."
-</p>
-<p>
-  "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut
-  labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris
-  nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit
-  esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt
-  in culpa qui officia deserunt mollit anim id est laborum."
-</p>
-<p>
-  "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut
-  labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris
-  nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit
-  esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt
-  in culpa qui officia deserunt mollit anim id est laborum."
-</p>
-<p>
-  "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut
-  labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris
-  nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit
-  esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt
-  in culpa qui officia deserunt mollit anim id est laborum."
-</p>
-<p>
-  "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut
-  labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris
-  nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit
-  esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt
-  in culpa qui officia deserunt mollit anim id est laborum."
-</p>
+<script lang="typescript">
+  import Search from '$lib/components/Search.svelte';
+</script>
+
+<svelte:head>
+  <title>Invoices | The Dollar Holler</title>
+</svelte:head>
+
+<!-- list of invoices -->
+<div>
+  <Search />
+
+  <!-- new invoice button -->
+  <div>
+    <button
+      class="relative translate-y-0 whitespace-nowrap rounded-lg bg-lavenderIndigo px-10 py-3 font-sansSerif text-xl font-black text-white shadow-colored transition-all hover:-translate-y-2 hover:shadow-coloredHover"
+      >+ Invoice</button
+    >
+  </div>
+  <div>
+    <h3>Status</h3>
+    <h3>Due Date</h3>
+    <h3>ID</h3>
+    <h3>Client</h3>
+    <h3>Amoung</h3>
+    <!-- view icon -->
+    <div />
+    <!-- three dots -->
+    <div />
+  </div>
+</div>
+
+<!-- invoices -->
+<div>
+  <div>Tag</div>
+  <div>8/1/2023</div>
+  <div>12345</div>
+  <div>Compressed.fm</div>
+  <div>504.00$</div>
+</div>
+
+<div>Total amount: 4.500$</div>
